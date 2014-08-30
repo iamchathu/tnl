6120876c9697a80b0e0978ce80ef8207c0442c5a
pasindud <ghosteyes17@ymail.com>
2014-08-31 05:09:22 +0530

    init

---
+++ app.js
@@ -0,0 +1,134 @@
+
+var express = require('express'),
+app = module.exports = express.createServer();
+io = require('socket.io').listen(app, { log: false });
+var fs = require('fs');
+//var redis = require("redis");
+
+//var pub = redis.createClient();
+  //pub.publish("messages", JSON.stringify({type: "foo", content: "bar"}));
+
+  //app.use(express.static('public'));
+
+  app.set('view engine', 'ejs');
+  app.set("views", __dirname + "/views");
+  // app.set("view engine", "jade");
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(app.router);
+  app.use(express["static"](__dirname + "/public"));
+
+
+  app.configure("development", function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+
+  var GobaleConnNum=0;
+  var sk='';
+  io.sockets.on('connection', function (socket) {
+      sk=socket;
+      GobaleConnNum++;
+
+    sk.emit('GobaleConnNum',GobaleConnNum);
+
+    socket.on('adminon', function (data) {
+      sk.broadcast.emit('adminin',data);
+      sk.emit('adminin', data); 
+    });
+
+    socket.on('admin_enable_module_audiostream', function (data) {
+      console.log("admin_enable_module_audiostream");
+      sk.broadcast.emit('module_audio_stream_on',data);
+      sk.emit('module_audio_stream_on', data); 
+    });
+
+    socket.on('admin_enable_module_audiostream_slef', function (data) {
+      sk.broadcast.emit('set_enable_module_audiostream_slef',data);
+      sk.emit('set_enable_module_audiostream_slef', data); 
+        // console.log(data);
+    });
+
+    socket.on('adminLogon', function (data) {
+      var file = 'keys.json';
+      fs.readFile(file, 'utf8', function (err, fileData) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }else{
+          sk.emit('adminLogonKeyDetails', JSON.parse(fileData)); 
+          console.log(data);
+        }
+      });
+
+      fs.readFile("data/data.json", 'utf8', function (err, fileData) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }else{
+          sk.emit('adminLogonKeyDetailsChoregpy', JSON.parse(fileData)); 
+          //console.log(data);
+        }
+      });
+
+
+    });
+
+
+    socket.on('dj_module', function (data) {
+      sk.broadcast.emit('send_emit',data);
+      sk.emit('send_emit', data); 
+    });
+
+
+    socket.on('dj_module_save', function (data) {
+      var outputFilename = 'keys.json';
+      fs.writeFile(outputFilename, JSON.stringify(data, null, 4), function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("JSON saved to " + outputFilename);
+        }
+      });
+    });
+
+
+    socket.on('dj_module_send_color', function (data) {
+      sk.broadcast.emit('dj_module_get_color',data);
+      sk.emit('dj_module_get_color', data); 
+    });
+
+
+    socket.on('chorgrapyingPropagate', function (data) {
+      sk.broadcast.emit('chorgrapyingPropagate_Client',data);
+      sk.emit('chorgrapyingPropagate_Client', data); 
+    });
+
+    socket.on('chorgrapyingRub', function (data) {
+      sk.broadcast.emit('chorgrapyingRub_Client',data);
+      sk.emit('chorgrapyingRub_Client', data); 
+    });
+
+
+
+  });
+
+/*
+* @description - Render the simulator
+*/
+app.get('/', function(req,res){
+  res.render('ejs/index',{})
+});
+
+app.get('/admin', function(req,res){
+ res.render('ejs/admin',{});
+});
+
+
+var port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log("Express server listening on port %d in mode", app.address().port);
---
+++ public/js/crowdsourcewebrtc/webrtcController.js
@@ -0,0 +1,102 @@
+navigator.getWebcam = (
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
+);
+
+// Peerjs content
+
+var peer = new Peer({
+    key: 'mv007pe6rw7phkt9',
+    debug: 3,
+    config: {
+        'iceServers': [
+            {
+                url: 'stun:stun.l.google.com:19302'
+            },
+            {
+                url: 'stun:stun1.l.google.com:19302'
+            },
+            {
+                url: 'turn:numb.viagenie.ca',
+                username: "lisa@learnfromlisa.com",
+                credential: "22paris22"
+            }
+    ]
+    }
+});
+
+// On open, set the peer id
+peer.on('open', function () {
+    $('#my-id').text(peer.id);
+});
+
+peer.on('call', function (call) {
+    // Answer call automatically
+    call.answer(window.localStream);
+    step3(call);
+})
+
+// Click handlers setup
+$(function () {
+    $('#make-call').click(function () {
+        // Initiate call
+        var callerId = $('#callto-id').val();
+        var call = peer.call($('#callto-id').val(), window.localStream);
+        step3(call);
+    });
+
+    $('end-call').click(function () {
+        window.existingCall.close();
+        step2();
+    });
+
+    // Retry if fails
+    $('#step1-retry').click(function () {
+        $('#step1-error').hide();
+        step();
+    });
+
+    // Get started
+    step1();
+})
+
+function step1() {
+    // Get webcam
+    navigator.getWebcam({
+        audio: false,
+        video: true
+    }, function (stream) {
+        // Display video stream
+        $('#my-video').prop('src', URL.createObjectURL(stream));
+
+        window.localStream = stream;
+        step2();
+    }, function () {
+        $('#step1-error').show();
+    });
+}
+
+
+function step2() {
+    // Adjust the UI
+    $('#step1', '#step3').hide();
+    $('#step2').show();
+}
+
+function step3(call) {
+    // Hang up on the existing call if present
+    if (window.existingCall) {
+        window.existingCall.close();
+    }
+
+    // Wait for the stream on, then setup peer video
+    call.on('stream', function (stream) {
+        $('#remotes').append("<video muted='true' src='" + URL.createObjectURL(stream) + "' autoplay></video>");
+//        $('#their-video').prop('src', URL.createObjectURL(stream));
+    });
+
+    $('#step1', '#step2').hide();
+    $('#step3').show();
+}
