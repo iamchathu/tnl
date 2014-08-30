e3991421bdcf390bfccee4378f897a8824706340
pasindud <ghosteyes17@ymail.com>
2014-08-30 20:55:15 +0530

    added dynamic include admins audio

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,7 +4,14 @@ var socket = io.connect('hacknrollkiss.herokuapp.com');
 var chorgrapydata ;  
 socket.emit('working', { });
 
-$('audio').AudioPlayerV1('pause');
+var defaulurl ="http://192.99.240.255:8010/live.mp3";
+
+
+// $( document ).ready(function() {
+  // var ply = document.getElementById('player');
+  // var oldSrc = ply.src;
+  // ply.src = "";
+// });
 
 $( "#adminon" ).click(function() {
   socket.emit('adminon', { });
@@ -22,8 +29,15 @@ $( "#chorgrapyingRub" ).click(function() {
 var livemodule="";
 
 $( "#enableaudion" ).click(function() {
-  
-document.getElementById('player').play();
+  console.log("nak");
+  // var ply = document.getElementById('player');
+  // var oldSrc = ply.src;
+  // ply.src = defaulurl;
+
+  //$("#player").attr("src",defaulurl);
+
+  $(".playloc").html(' <audio   id="player"  src="http://192.99.240.255:8010/live.mp3" controls="" autoplay="" preload autobuffer></audio>');
+  document.getElementById('player').play();
 
   var visualizer = new Visualizer();
   var player =  document.getElementById('player');
